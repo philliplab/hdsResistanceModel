509de90947cf960b741579f82ae9f3ecc991a47f
Phillipl <philliplab@gmail.com>
2013-10-18 11:58:38 +0200

    ADDS lots of scenario specifications

--- R/get_scenario.R
+++ R/get_scenario.R
@@ -26,6 +26,195 @@ get_scenario <- function(scenario_name){
              2, 0, 2,
              2, 2, 0)
   )
+  
+  userParsLib[['Simple_2_1']] <- list(
+   timeStep = 0.1,
+   timeStop = 1500,
+   systemName = "Simple_2_1",
+   systemDescription = "Two wild type viruses that can stochastically mutate into 1 target strain. 
+   This allows for 1 stochastic events and 1 'path' through the system. 
+   No treatment effect but strain 3 is more fit than strains 1 and 2",
+   Pf = c(0.95, 0.949, 1), # Fitnesses of the different strains
+   treatments = list(list(t = 0, Te = 0.0, Ts = c(0.8, 0.80, 0.75))),
+   mutationAcceleration = 1.5*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 3, # Number of strains in system
+   offStrains = c(3), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0, 2, 2,
+            2, 0, 2,
+            2, 2, 0)
+  )
+  
+  userParsLib[['Simple_2_2']] <- list(
+   timeStep = 0.5,
+   timeStop = 2500,
+   systemName = "Simple_2_2",
+   systemDescription = "Two wild type viruses that can stochastically mutate into a different a target strain. 
+   This allows for 2 stochastic events and 2 'paths' through the system. 
+   No treatment effect but strains 2 and 4 are more fit than strains 1 and 3",
+   Pf = c(0.95, 1, 0.951, 1), # Fitnesses of the different strains
+   treatments = list(list(t = 0, Te = 0.0, Ts = c(0.8, 0.80, 0.75, 0.8))),
+   mutationAcceleration = 2*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 4, # Number of strains in system
+   offStrains = c(2,4), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0,   2,   Inf, Inf,
+            2,   0,   Inf, Inf,
+            Inf, Inf, 0,   2,
+            Inf, Inf, 2,   0)
+  )
+  
+  userParsLib[['RepRes_1_1']] <- list(
+   timeStep = 0.5,
+   timeStop = 2500,
+   systemName = "RepRes_1_1",
+   systemDescription = "Two strains that can mutate into each other. An attempt to show repeated ressurection",
+   Pf = c(0.85, 0.95), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.0, Ts = c(0.8, 0.80))),
+   mutationAcceleration = 2*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 2, # Number of strains in system
+   offStrains = c(2), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0,   2,
+            2,   0)
+  )
+  
+  userParsLib[['Simple_1_0']] <- list(
+   timeStep = 0.5,
+   timeStop = 2500,
+   systemName = "Simple_1_0",
+   systemDescription = "One strains that cannot mutate. An attempt to illustrate varying levels of treatment",
+   Pf = c(0.85), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0, Ts = c(0.8)),
+                     list(t=500, Te = 0.1, Ts = c(0.8)),
+                     list(t=1000, Te = 0.0, Ts = c(0.8)),
+                     list(t=1500, Te = 0.2, Ts = c(0.8))
+                     ),
+   mutationAcceleration = 2*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 1, # Number of strains in system
+   offStrains = 0[0], # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return (0.4)}, # draw this number randomly in production situation
+   Epow = c(0)
+  )
+  
+  userParsLib[['BadStart_1_1']] <- list(
+   timeStep = 0.5,
+   timeStop = 2500,
+   systemName = "BadStart_1_1",
+   systemDescription = "Initial infection with resistent strain. Untreated until wild type pushes resistent strain to very low population levels. Treatment is initiated causing the initial resistence to arise again",
+   Pf = c(0.85, 1), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.0, Ts = c(0.8, 0.8)),
+                     list(t=1985, Te = 0.3, Ts = c(0, 1))
+                     ),
+   mutationAcceleration = 2*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 2, # Number of strains in system
+   offStrains = c(2), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0,   2,
+            2,   0)
+  )
+  
+  userParsLib[['PoorTreat_1_1']] <- list(
+   timeStep = 0.5,
+   timeStop = 3500,
+   systemName = "PoorTreat_1_1",
+   systemDescription = "FAILURE: Initial infection with wildType Strain. Resistent mutations occur, but are driven to extinction by dominance of wildType. Poor treatment is initiated allowing drug resistent strain to flourish",
+   Pf = c(1, 0.90), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.0, Ts = c(0.8, 0.8)),
+                     list(t=1000, Te = 0.18, Ts = c(1,0))
+                     ),
+   mutationAcceleration = 2.5*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 2, # Number of strains in system
+   offStrains = c(2), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0,   1,
+            1,   0)
+  )
+  
+  userParsLib[['AccuRes_1_2']] <- list(
+   timeStep = 0.5,
+   timeStop = 3500,
+   systemName = "AccuRes_1_2",
+   systemDescription = "3 strain system. Strain 1 very fit, but susceptible to treatment; strain 2 med fit, med susceptibility; strain 3: low fit los suscept",
+   Pf = c(1, 0.95, 0.94), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.0, Ts = c(0.8, 0.8, 0.8)),
+                     list(t=1000, Te = 0.1, Ts = c(1, 0.3, 0.0))
+                     ),
+   mutationAcceleration = 2.5*(10^(-1)), # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 3, # Number of strains in system
+   offStrains = c(2,3), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0,   2,   3,
+            2,   0,   1,
+            3,   1,   0)
+  )
+  
+  userParsLib[['AccuTams_1_4']] <- list(
+   timeStep = 0.5,
+   timeStop = 3000,
+   systemName = "AccTams_1_4",
+   systemDescription = "5 strain system. Going from very fit but susceptible to treatment to (roughly) moderate fitness but high resistance to treatment",
+   Pf = c(0.99, 0.96, 0.92, 0.91, 1), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.19, Ts = c(1, 0.75, 0.5, 0.1, 0))
+                     ),
+   mutationAcceleration = 1/200, # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 5, # Number of strains in system
+   offStrains = c(2,3,4,5), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0, 1, 2, 3, 4,
+            1, 0, 1, 2, 3,
+            2, 1, 0, 1, 2,
+            3, 2, 1, 0, 1,
+            4, 3, 2, 1, 0)
+  )
+  
+  userParsLib[['AccuTams_1_3']] <- list(
+   timeStep = 0.5,
+   timeStop = 3000,
+   systemName = "AccTams_1_3",
+   systemDescription = "4 strain system. Going from very fit but susceptible to treatment to (roughly) moderate fitness but high resistance to treatment",
+   Pf = c(0.99, 0.95, 0.93, 0.99), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0.19, Ts = c(1, 0.75, 0.5, 0))
+                     ),
+   mutationAcceleration = 1/200, # adjustment to make the timescales reasonable
+   Td = 0.2, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 4, # Number of strains in system
+   offStrains = c(2,3,4), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){return(runif(1))}, # draw this number randomly in production situation
+   Epow = c(0, 1, 2, 3,
+            1, 0, 1, 2,
+            2, 1, 0, 1,
+            3, 2, 1, 0)
+  )
+  
+  userParsLib[['Systematic_Buildup']] <- list(
+   timeStep = 1,
+   timeStop = 10000,
+   systemName = "Systematic_Buildup",
+   systemDescription = "A Systematic build-up starting with one strain and adding ones slowly",
+   Pf = c(0.5000003, 0.5000034), # Fitnesses of the different strains
+   treatments = list(list(t=0, Te = 0, Ts = c(0))
+                     ),
+   mutationAcceleration = 1, # adjustment to make the timescales reasonable
+   Td = 0.5, # Tcell depletion - ratio of pre-infected to post-infected equilibria
+   N_S = 2, # Number of strains in system
+   offStrains = c(2), # Strains not present in the initial system
+   stochasticEventThresholdSource = function(){runif(1)}, # draw this number randomly in production situation
+   Epow = c(0, 1,
+            1, 0)
+  )
+  
+  
+  
+  
   if (scenario_name %in% names(userParsLib)){
     return(do.call(scenario, userParsLib[[scenario_name]]))
   } else {
