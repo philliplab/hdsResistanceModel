0134c0cd94829479f21ac712d5180d536a7156de
Phillip Labuschagne <philliplab@gmail.com>
2013-11-05 09:49:04 +0200

    ADDS more tests for deathThreshold toggling

--- inst/tests/test-de_system.R
+++ inst/tests/test-de_system.R
@@ -11,6 +11,16 @@ test_that('Changing the deathThreshold via get_scenario works',{
   stable_pops <- compute_stable_populations(ss, compari = 5)
   expect_that(names(stable_pops[[1]]), equals("0.04995"))
 
+  scenario <- get_scenario('AccuTams_1_2', modified_parameters = list(deathThreshold = 0.001))
+  ss <- run_system(scenario, 1)
+  stable_pops <- compute_stable_populations(ss, compari = 5)
+  expect_that(names(stable_pops[[3]]), equals("0.001"))
+
+  scenario <- get_scenario('AccuTams_1_2', modified_parameters = list(deathThreshold = 0.0001))
+  ss <- run_system(scenario, 1)
+  stable_pops <- compute_stable_populations(ss, compari = 5)
+  expect_that(names(stable_pops[[3]]), equals("1e-04"))
+
 })
 
 test_that('Changing the offThreshold via get_scenario works',{
